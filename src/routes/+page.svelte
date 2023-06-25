34d9131739a7b23ca2e51139206479ce5c2782bf
Jakku Night <J97k107u45k117n@gmail.com>
2023-06-25 16:41:53 -0400

    Soporte para GH-Pages añadido.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let count = 0;
+    let msg = "Púlsame!"
+    const handleClick = (event) => {
+        count = count + 1;
+        msg = "Me han pulsado " + count + " veces!";
+    };
+</script>
+<button on:click={handleClick}>
+    {msg}
+</button>
